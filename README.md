994a7d1be5a28bb6bb940b3e3b650516c00b6d42
Alex Van Camp <email@alexvan.camp>
2014-09-14 21:59:48 -0500

    REAL initial commit!

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-barry-donations
-===============
+# barry-donations
+An evented API wrapper for [Barry's Donation Tracker](http://don.barrycarlyon.co.uk/), a service that makes tracking donations 
+easier for Twitch broadcasters.
+
+### Installation
+Install [node.js & npm](http://nodejs.org/), then install [bower](http://bower.io/), then run the following:
+```
+npm install barry-donations
+```
+
+### Usage
+```javascript
+var BarryDonations = require('barry-donations');
+
+var options = {
+    username: "testuser",
+    password: "science",
+    version: "dansoverlay1"
+};
+var bd = new BarryDonations(options);
+
+bd.on('initialized', function (data, lasttos) {
+    console.log("[init]" + data);
+});
+bd.on('newdonations', function (data, lasttos) {
+    console.log("[newdonations] " + data);
+});
+```
+
+### Contributing
+1. Fork it ( http://github.com/langeh/barry-donations/fork )
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+### License
+barry-donations is provided under the MIT license, which is available to read in the [LICENSE][] file.
+
+### Credits
+[Barry Carlyon](http://barrycarlyon.co.uk/), developer of [Barry's Donation Tracker](http://don.barrycarlyon.co.uk/)
---
+++ index.js
@@ -0,0 +1,120 @@
+var request = require('request'),
+    util = require("util"),
+    EventEmitter = require("events").EventEmitter;
+
+function BarryDonations(options) {
+    EventEmitter.call(this);
+
+    this.options = options;
+
+    this.validate();
+}
+
+util.inherits(BarryDonations, EventEmitter);
+
+BarryDonations.prototype.validate = function() {
+    var self = this;
+
+    var url = 'http://don.barrycarlyon.co.uk/api.php?method=validate' +
+        '&username=' + this.options.username +
+        '&password=' + this.options.password +
+        '&version=' + this.options.version;
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var bodyJSON = JSON.parse(body);
+
+            if (bodyJSON.status !== "ok") {
+                console.error("[BARRY-DONATIONS] Failed to validate, API returned status: " + bodyJSON.status);
+                return;
+            }
+
+            self.settings = bodyJSON.settings;
+
+            self.init();
+        } else {
+            console.error("[BARRY-DONATIONS] Failed to validate: " + error);
+        }
+    });
+};
+
+BarryDonations.prototype.init = function() {
+    var self = this;
+
+    var url = 'http://don.barrycarlyon.co.uk/api.php?method=initial' +
+        '&username=' + this.options.username +
+        '&password=' + this.options.password +
+        '&version=' + this.options.version;
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var bodyJSON = JSON.parse(body);
+
+            var latest = 0;
+            bodyJSON.data.Completed.forEach(function(donation) {
+                if (donation.utos > latest) {
+                    latest = donation.utos;
+                }
+            });
+            self.options.lasttos = latest;
+
+            self.emitInit(bodyJSON.data, latest);
+
+            //kill any existing fetch timers
+            self.kill();
+
+            //fetch new data (delta) from the api every 30 seconds
+            self._fetchtimer = setInterval(self.fetch, 30000, self);
+        } else {
+            console.error("[BARRY-DONATIONS] Failed to get initial data: " + error);
+        }
+    });
+};
+
+BarryDonations.prototype.fetch = function(scope) {
+    var url = 'http://don.barrycarlyon.co.uk/api.php?method=update' +
+        '&username=' + scope.options.username +
+        '&password=' + scope.options.password +
+        '&version=' + scope.options.version +
+        '&lasttos=' + scope.options.lasttos; //make sure we fetch this freshly from
+
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var bodyJSON = JSON.parse(body);
+
+            // this will be true if any donations have a newer timestamp than "lasttos"
+            if (bodyJSON.flag === false) {
+                return;
+            }
+
+            var latest = 0;
+            bodyJSON.data.Completed.forEach(function(donation) {
+                if (donation.utos > latest) {
+                    latest = donation.utos;
+                }
+            });
+            scope.options.lasttos = latest;
+
+            scope.emitNewDonations(bodyJSON.data, latest);
+        } else {
+            console.error("[BARRY-DONATIONS] Failed to fetch update: " + error);
+        }
+    });
+};
+
+BarryDonations.prototype.kill = function() {
+    if(this._fetchtimer !== null) {
+        clearInterval(this.fetchtimer);
+        this._fetchtimer = null;
+    }
+};
+
+BarryDonations.prototype.emitInit = function(data, lasttos) {
+    this.emit("initialized", data, lasttos);
+};
+
+BarryDonations.prototype.emitNewDonations = function(data, lasttos) {
+    this.emit("newdonations", data, lasttos);
+};
+
+module.exports = BarryDonations;
